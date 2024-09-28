c0fd9d63a74c92176069d512087a2a222d0e26e8
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2024-09-28 09:31:40 +0200

    Updating <Scriptable> from my laptop (2024-09-28 09:31:39)
    Affected file(s) (only up to 20 files are shown):
    widget_journal-prompt.js

--- widget_journal-prompt.js
+++ widget_journal-prompt.js
@@ -9,7 +9,7 @@ widget.useDefaultPadding();
 
 widget.addSpacer(20);
 
-let journalPrompt = widget.addText("What would my 80-year-old self say about my decisions today?");
+let journalPrompt = widget.addText("Imagine you're near death and in a coma at the hospital, everybody has gathered around you and you can hear them. But they don't know that you can hear them and start talking about you. What do you want them to be saying?");
 journalPrompt.centerAlignText();
 journalPrompt.textColor = new Color("#ffffff");
 // http://iosfonts.com
