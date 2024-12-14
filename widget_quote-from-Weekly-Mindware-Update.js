976ec51f92d6051a23890b776489caa77fcc6a29
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2024-12-14 18:39:49 +0100

    Updating <Scriptable> from my laptop (2024-12-14 18:39:49)
    Affected file(s) (only up to 10 files are shown):
    widget_quote-from-Weekly-Mindware-Update.js

--- widget_quote-from-Weekly-Mindware-Update.js
+++ widget_quote-from-Weekly-Mindware-Update.js
@@ -1,5 +1,8 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
+// icon-color: brown; icon-glyph: magic;
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
 // icon-color: gray; icon-glyph: smile-wink;
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
