36c481761b3a76960c570e21a0a5a871870da423
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2025-01-11 19:32:52 +0100

    Updating <Sciptable> from my iPhone (2025-01-11 19:32:51)

--- widget_quote-from-Weekly-Mindware-Update.js
+++ widget_quote-from-Weekly-Mindware-Update.js
@@ -1,5 +1,11 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
+// icon-color: yellow; icon-glyph: magic;
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: orange; icon-glyph: magic;
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
 // icon-color: gray; icon-glyph: smile-wink;
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
