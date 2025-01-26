d7e5f358c7648d0f3d86b12e127d2e73fc180ccd
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2025-01-26 09:26:59 +0800

    Updating <Scriptable> from my MacBook (2025-01-26 09:26:59)
    Affected file(s) (only up to 10 files are shown):
    widget_quote-from-Weekly-Mindware-Update.js

--- widget_quote-from-Weekly-Mindware-Update.js
+++ widget_quote-from-Weekly-Mindware-Update.js
@@ -1,5 +1,8 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
+// icon-color: light-brown; icon-glyph: magic;
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
 // icon-color: gray; icon-glyph: smile-wink;
 // 📁 https://gitwhub.com/huaminghuangtw/Weekly-Mindware-Update
 const utils = importModule("utils");
