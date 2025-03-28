8314f442ee9f9ea69ecb37ea2e00ee489eada872
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2025-03-28 09:03:22 +0100

    Updating <Scriptable> from my MacBook (2025-03-28 09:03:22)
    Affected file(s) (only up to 10 files are shown):
    "Create Notification.js"
    utils.js
    widget_100-blocks-a-day.js
    widget_dear-today-me.js
    widget_display-book-excerpts.js
    widget_journal-prompt.js
    widget_quote-from-Weekly-Mindware-Update.js
    widget_quote-from-ZenQuotes-API.js

--- Create Notification.js
+++ Create Notification.js
@@ -5,26 +5,33 @@ const inputs = args.shortcutParameter;
 
 const notification = new Notification();
 
-notification.identifier = inputs.id ? inputs.id : (inputs.title ? inputs.title : "");
+notification.identifier = inputs.id
+    ? inputs.id
+    : inputs.title
+    ? inputs.title
+    : "";
 
 if (inputs.threadId) notification.threadIdentifier = inputs.threadId;
 if (inputs.title) notification.title = inputs.title;
 
 const textDivider = "──────────────";
 
-notification.subtitle = inputs.subtitle 
-    ? `${textDivider}\n${inputs.subtitle}\n${textDivider}` 
+notification.subtitle = inputs.subtitle
+    ? `${textDivider}\n${inputs.subtitle}\n${textDivider}`
     : textDivider;
 
 if (inputs.body) notification.body = inputs.body;
 if (inputs.openURL) notification.openURL = inputs.openURL;
-if (inputs.triggerDate) notification.setTriggerDate(new Date(inputs.triggerDate));
+if (inputs.triggerDate)
+    notification.setTriggerDate(new Date(inputs.triggerDate));
 
-const actions = inputs.actions 
-     ? JSON.parse(`[${inputs.actions.replace(/\n/g, ",")}]`) 
-     : (inputs.openURL ? [{ title: "🔗 Open URL", url: inputs.openURL }] : []);
+const actions = inputs.actions
+    ? JSON.parse(`[${inputs.actions.replace(/\n/g, ",")}]`)
+    : inputs.openURL
+    ? [{ title: "🔗 Open URL", url: inputs.openURL }]
+    : [];
 
-actions.forEach(action => {
+actions.forEach((action) => {
     notification.addAction(action.title, action.url);
 });
 
