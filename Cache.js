4e7fd4a558509282525ae0a7309cbc77940b409a
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2025-04-05 09:42:53 +0200

    Updating <Scriptable> from my MacBook (2025-04-05 09:42:53)
    Affected file(s) (only up to 10 files are shown):
    Cache.js
    TerminalWidget.js

--- Cache.js
+++ Cache.js
@@ -1,3 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-blue; icon-glyph: magic;
 // NOTE: This script was written by evandcoleman: https://github.com/evandcoleman/scriptable
 
 class Cache {
