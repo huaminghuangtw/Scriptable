50360e1bd49689c0b8d3ec9a733824883d737aeb
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2025-08-05 16:14:42 +0800

    Update: widget_journal-prompt.js

--- widget_journal-prompt.js
+++ widget_journal-prompt.js
@@ -44,7 +44,7 @@ async function fetchRandomJournalPrompt() {
             ).subtasks
         ).name
     );
-    
+
     return randomJournalPrompt;
 }
 
@@ -64,12 +64,15 @@ async function createWidget(randomJournalPrompt) {
     text.textOpacity = CONFIG.TEXT_OPACITY;
     text.textColor = CONFIG.TEXT_COLOR;
 
-    let prompt = "If you were in my shoes, how would you answer this question? Get specific and be brief. Use less than 100 words." + "\n" + randomJournalPrompt;
-    
+    // 👉 Download this shortcut: https://shortcutomation.com/gallery/api-gemini/gemini-generate-content
     widget.url =
         `shortcuts://run-shortcut?` +
         `name=${encodeURIComponent("Gemini - Generate Content")}&` +
-        `input=${encodeURIComponent(prompt)}`;
+        `input=${encodeURIComponent(
+            "If you were in my shoes, how would you answer this question? Get specific and be brief. Use less than 100 words." +
+                "\n" +
+                randomJournalPrompt
+        )}`;
 
     return widget;
 }
