1b1c87bba4f4a593b134790cf6b2fd3c5bc0e32a
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2026-05-21 20:02:53 +0800

    Update: widget_quote-from-Weekly-Mindware-Update.js

--- widget_quote-from-Weekly-Mindware-Update.js
+++ widget_quote-from-Weekly-Mindware-Update.js
@@ -1,12 +1,15 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: gray; icon-glyph: smile-wink;
-// 📁 https://github.com/huaminghuangtw/Weekly-Mindware-Update
+// 💁‍♂️ https://github.com/huaminghuangtw/Weekly-Mindware-Update
 const CONFIG = {
-    FONT: { NAME: "IowanOldStyle-BoldItalic", SIZE: 18 },
-    MINIMUM_SCALE_FACTOR: 0.1,
-    TEXT_OPACITY: 1,
-    TEXT_COLOR: Color.white(),
+  FONT: {
+    NAME: "IowanOldStyle-BoldItalic",
+    SIZE: 18,
+  },
+  MINIMUM_SCALE_FACTOR: 0.1,
+  TEXT_OPACITY: 1,
+  TEXT_COLOR: Color.white(),
 };
 
 const Utils = importModule("Utils");
@@ -27,51 +30,49 @@ Script.complete();
 // ================
 
 async function fetchRandomQuote() {
-    let files = await Utils.getRepoTree(
-        "huaminghuangtw",
-        "Weekly-Mindware-Update"
-    );
+  let files = await Utils.getRepoTree(
+    "huaminghuangtw",
+    "Weekly-Mindware-Update",
+  );
 
-    let filePath = Utils.getRandomItem(
-        files.filter(
-            (f) => f.path.includes("issues") && f.path.endsWith(".md")
-        )
-    ).path;
+  let filePath = Utils.getRandomItem(
+    files.filter((f) => f.path.includes("issues") && f.path.endsWith(".md")),
+  ).path;
 
-    let fileContent = await Utils.getFileContent(
-        "huaminghuangtw",
-        "Weekly-Mindware-Update",
-        filePath
-    );
+  let fileContent = await Utils.getFileContent(
+    "huaminghuangtw",
+    "Weekly-Mindware-Update",
+    filePath,
+  );
 
-    let sectionContent = fileContent
-        .split("\n")
-        .filter((line) => line.startsWith("*"))
-        .map((line) => line.slice(1).trim())
-        .slice(0, 5);
+  let sectionContent = fileContent
+    .split("\n")
+    .filter((line) => line.startsWith("*"))
+    .map((line) => line.slice(1).trim())
+    .slice(0, 5);
 
-    let randomQuote = Utils.convertMarkdownToPlainText(
-        Utils.getRandomItem(sectionContent)
-    );
+  let randomQuote = Utils.convertMarkdownToPlainText(
+    Utils.getRandomItem(sectionContent),
+  );
 
-    return randomQuote;
+  return randomQuote;
 }
 
-async function createWidget(randomQuote) {
-    let widget = new ListWidget();
+async function createWidget(t) {
+  let widget = new ListWidget();
 
-    let text = widget.addText(Utils.truncateText(randomQuote));
-    text.centerAlignText();
-    text.font = new Font(CONFIG.FONT.NAME, CONFIG.FONT.SIZE);
-    text.minimumScaleFactor = CONFIG.MINIMUM_SCALE_FACTOR;
-    text.textOpacity = CONFIG.TEXT_OPACITY;
-    text.textColor = CONFIG.TEXT_COLOR;
+  let text = widget.addText(Utils.truncateText(t));
+  text.centerAlignText();
+  text.font = new Font(CONFIG.FONT.NAME, CONFIG.FONT.SIZE);
+  text.minimumScaleFactor = CONFIG.MINIMUM_SCALE_FACTOR;
+  text.textOpacity = CONFIG.TEXT_OPACITY;
+  text.textColor = CONFIG.TEXT_COLOR;
 
-    // 👉 Download this shortcut: https://shortcutomation.com/add-to-inbox
-    widget.url =
-        `shortcuts://run-shortcut?` +
-        `name=${encodeURIComponent("📥 Add to Inbox")}&` +
-        `input=${encodeURIComponent(randomQuote)}`;
+  // 👉 Download this shortcut: https://shortcutomation.com/add-to-inbox
+  widget.url =
+    `shortcuts://run-shortcut?` +
+    `name=${encodeURIComponent("📥 Add to Inbox")}&` +
+    `input=${encodeURIComponent(t)}`;
 
-    return widget;
+  return widget;
 }
