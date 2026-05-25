61430f85086afb99da160494fc93cc7ed8d2104b
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2026-05-25 18:10:14 +0800

    Update: widget_dear-today-me.js

--- widget_dear-today-me.js
+++ widget_dear-today-me.js
@@ -21,7 +21,7 @@ let randomParagraph = await cache.getOrFetch(fetchRandomParagraph);
 
 let widget = await createWidget(randomParagraph);
 
-config.runsInWidget ? Script.setWidget(widget) : widget.presentMedium();
+config.runsInWidget ? Script.setWidget(widget) : widget.presentLarge();
 
 Script.complete();
 
