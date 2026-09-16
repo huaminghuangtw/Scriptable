d078bd995fe382f225645be5369c5c5b5e30848c
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2026-09-16 20:26:47 +0800

    Update: Create-Notification.js

--- Create-Notification.js
+++ Create-Notification.js
@@ -5,37 +5,25 @@ const inputs = args.shortcutParameter;
 
 const notification = new Notification();
 
-notification.identifier = inputs.id
-    ? inputs.id
+notification.identifier = inputs.body
+    ? inputs.body
     : inputs.title
     ? inputs.title
     : "";
 
 if (inputs.threadID) notification.threadIdentifier = inputs.threadID;
 
-notification.title = inputs.title
-    .split("\n")
-    .map((line) => line.trim())
-    .filter((line) => line !== "")
-    .join("\n");
+notification.title =  inputs.title ? inputs.title : "‏‎‎‏";
 
 const textDivider = "──────────────";
 
 if (inputs.subtitle) {
-    notification.subtitle = `${textDivider}\n${inputs.subtitle
-        .split("\n")
-        .map((line) => line.trim())
-        .filter((line) => line !== "")
-        .join("\n")}\n${textDivider}`;
-} else if (inputs.body) {
+    notification.subtitle = `${textDivider}\n${inputs.subtitle}\n${textDivider}`;
+} else if (inputs.title && inputs.body) {
     notification.subtitle = textDivider;
 }
 
-if (inputs.body)
-    notification.body = inputs.body.split("\n")
-           .map((line) => line.trim())
-           .filter((line) => line !== "")
-           .join("\n")
+if (inputs.body) notification.body = inputs.body
 
 if (inputs.openURL) notification.openURL = inputs.openURL;
 
